885dc16de0028224b7168e2acec97f7e7775bbe5
WizardJuan <niniylulu01@gmail.com>
2024-03-21 10:30:19 -0500

    loginenclase

--- lib/main.dart
+++ lib/main.dart
@@ -3,178 +3,321 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 void main() {
   runApp(MaterialApp(
-    theme: ThemeData(primaryColor: Colors.green),
+    theme: ThemeData(primaryColor: const Color.fromARGB(255, 0, 0, 0)),
     debugShowCheckedModeBanner: false,
-    home: LoginPage(),
+    initialRoute: '/',
+    routes: {
+      '/': (context) => LoginPage(),
+      '/register': (context) => RegisterPage(),
+    },
   ));
 }
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  bool isRegistered = false;
+  bool isLoggedIn = false;
+  String? registeredEmail;
+  String? registeredPassword;
+
+  void register() async {
+    final result = await Navigator.pushNamed(context, '/register');
+    if (result != null && result is Map<String, String>) {
+      setState(() {
+        isRegistered = true;
+        registeredEmail = result['email'];
+        registeredPassword = result['password'];
+      });
+    }
+  }
+
+  void login() {
+    if (isRegistered) {
+      final enteredEmail = emailController.text;
+      final enteredPassword = passwordController.text;
+
+      if (enteredEmail == registeredEmail && enteredPassword == registeredPassword) {
+        setState(() {
+          isLoggedIn = true;
+        });
+      } else {
+        showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: Text('Error de inicio de sesión'),
+              content: Text('Credenciales incorrectas. Verifica tu correo y contraseña.'),
+              actions: <Widget>[
+                TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: Text('Cerrar'),
+                ),
+              ],
+            );
+          },
+        );
+      }
+    } else {
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text('Error de inicio de sesión'),
+            content: Text('Debes registrarte antes de iniciar sesión.'),
+            actions: <Widget>[
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: Text('Cerrar'),
+              ),
+            ],
+          );
+        },
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: Container(
-          height: MediaQuery.of(context).size.height,
-          width: MediaQuery.of(context).size.width,
-          decoration: BoxDecoration(
+    if (isLoggedIn) {
+      return HomePage();
+    } else {
+      return Scaffold(
+        body: SingleChildScrollView(
+          child: Container(
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+            decoration: BoxDecoration(
+              // Cambiar el color a negro
               gradient: LinearGradient(
-                  begin: Alignment.topLeft,
-                  end: Alignment.bottomRight,
-                  colors: [
-                Color(0xFF50C878),
-                Color(0xFF00FF00),
-                Color(0xFF006400),
-              ])),
-          child:
-              Column(crossAxisAlignment: CrossAxisAlignment.center, children: [
-            SizedBox(
-              height: 10,
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors: [
+                  Color.fromARGB(255, 0, 0, 0),
+                  Color.fromARGB(255, 0, 0, 0),
+                  Color.fromARGB(255, 0, 0, 0),
+                ],
+              ),
             ),
-            Image.asset('assets/logo.png'),
-            Container(
-              height: 500,
-              width: 325,
-              decoration: BoxDecoration(
-                  color: Colors.grey, borderRadius: BorderRadius.circular(10)),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  SizedBox(
-                    height: 30,
-                  ),         //xd
-                  Text(
-                    'Bienvenidos',
-                    style: TextStyle(
-                      fontSize: 35,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                    ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SizedBox(
+                  height: 10,
+                ),
+                Image.asset('assets/logo.png'),
+                Container(
+                  height: 500,
+                  width: 325,
+                  decoration: BoxDecoration(
+                    color: Color.fromARGB(255, 55, 64, 70),
+                    borderRadius: BorderRadius.circular(10),
                   ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Text(
-                    'Por favor, ingrese su cuenta',
-                    style: TextStyle(
-                      fontSize: 15,
-                      color: Colors.white,
-                    ),
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Container(
-                    width: 250,
-                    child: TextField(
-                      decoration: InputDecoration(
-                        labelText: 'Correo Electronico',
-                        suffixIcon: Icon(
-                          FontAwesomeIcons.envelope,
-                          size: 17,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      SizedBox(
+                        height: 30,
+                      ),
+                      Text(
+                        'Bienvenidos',
+                        style: TextStyle(
+                          fontSize: 35,
+                          fontWeight: FontWeight.bold,
+                          color: Color.fromARGB(255, 0, 136, 102),
                         ),
                       ),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Container(
-                    width: 250,
-                    child: TextField(
-                      decoration: InputDecoration(
-                        labelText: 'Correo Electronico',
-                        suffixIcon: Icon(
-                          FontAwesomeIcons.envelope,
-                          size: 17,
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Text(
+                        'Ingrese su cuenta',
+                        style: TextStyle(
+                          fontSize: 15,
+                          color: Colors.white,
                         ),
                       ),
-                    ),
-                  ),
-                  Container(
-                    width: 250,
-                    child: TextField(
-                      decoration: InputDecoration(
-                        labelText: 'Contraseña',
-                        suffixIcon: Icon(
-                          FontAwesomeIcons.eyeSlash,
-                          size: 17,
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Container(
+                        width: 250,
+                        child: TextField(
+                          controller: emailController,
+                          decoration: InputDecoration(
+                            labelText: 'Correo Electrónico',
+                            labelStyle: TextStyle(color: Colors.white),
+                            suffixIcon: Icon(
+                              FontAwesomeIcons.envelope,
+                              size: 17,
+                            ),
+                            hintStyle: TextStyle(color: Colors.white),
+                          ),
                         ),
                       ),
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.fromLTRB(20, 20, 20, 20),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          'Registrarse',
-                          style: TextStyle(color: Colors.white),
+                      Container(
+                        width: 250,
+                        child: TextField(
+                          controller: passwordController,
+                          decoration: InputDecoration(
+                            labelText: 'Contraseña',
+                            labelStyle: TextStyle(color: Colors.white),
+                            suffixIcon: Icon(
+                              FontAwesomeIcons.eyeSlash,
+                              size: 17,
+                            ),
+                            hintStyle: TextStyle(color: Colors.white),
+                          ),
+                          obscureText: true,
                         ),
-                        Text(
-                          'contraseña olvidada',
-                          style: TextStyle(color: Colors.white),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.fromLTRB(20, 20, 20, 20),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            GestureDetector(
+                              onTap: register,
+                              child: Text(
+                                'Registrarse',
+                                style: TextStyle(color: Colors.white),
+                              ),
+                            ),
+                            Text(
+                              '¿Contraseña olvidada?',
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          ],
                         ),
-                      ],
-                    ),
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  GestureDetector(
-                    child: Container(
-                      alignment: Alignment.center,
-                      width: 250,
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(50),
-                          gradient: LinearGradient(
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      GestureDetector(
+                        onTap: login,
+                        child: Container(
+                          alignment: Alignment.center,
+                          width: 250,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(50),
+                            gradient: LinearGradient(
                               begin: Alignment.centerLeft,
                               end: Alignment.centerRight,
                               colors: [
                                 Color(0xFF98FF98),
                                 Color(0xFF00FF00),
                                 Color(0xFF50C878),
-                              ])),
-                      child: Padding(
-                        padding: EdgeInsets.all(12.0),
-                        child: Text(
-                          'Iniciar Sesion',
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 20,
-                              fontWeight: FontWeight.bold),
+                              ],
+                            ),
+                          ),
+                          child: Padding(
+                            padding: EdgeInsets.all(12.0),
+                            child: Text(
+                              'Iniciar Sesión',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
                         ),
                       ),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 30,
-                  ),
-                  Text(
-                    'Iniciar Sesion con:',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                    ),
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      Icon(FontAwesomeIcons.facebookF, color: Colors.white),
-                      Icon(FontAwesomeIcons.solidEnvelope, color: Colors.white),
-                      Icon(FontAwesomeIcons.google, color: Colors.white),
+                      SizedBox(
+                        height: 30,
+                      ),
+                      Text(
+                        'Iniciar Sesión con:',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: const Color.fromARGB(255, 2, 2, 2),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Icon(FontAwesomeIcons.facebookF, color: Colors.white),
+                          Icon(FontAwesomeIcons.solidEnvelope, color: Colors.white),
+                          Icon(FontAwesomeIcons.google, color: Colors.white),
+                        ],
+                      ),
                     ],
                   ),
-                ],
-              ),
-            )
-          ]),
+                ),
+              ],
+            ),
+          ),
         ),
+      );
+    }
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Hola mundo'),
+      ),
+      body: Center(
+        child: Text('Bienvenido a la página de ejercicios'),
       ),
     );
   }
+}
+
+class RegisterPage extends StatelessWidget {
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Registro'),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextField(
+                controller: emailController,
+                decoration: InputDecoration(labelText: 'Correo Electrónico'),
+              ),
+              SizedBox(height: 20),
+              TextField(
+                controller: passwordController,
+                decoration: InputDecoration(labelText: 'Contraseña'),
+                obscureText: true,
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  final email = emailController.text;
+                  final password = passwordController.text;
+                  Navigator.pop(context, {'email': email, 'password': password});
+                },
+                child: Text('Registrarse'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
