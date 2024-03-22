e425ad93bb1691e89c980a06e06502a8df99c7fa
kirivanCode <ivansierravega8@gmail.com>
2024-03-22 09:52:41 -0500

    Estilo de registrar

--- lib/main.dart
+++ lib/main.dart
@@ -288,36 +288,112 @@ class RegisterPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Registro'),
+        backgroundColor: Colors.black, // Cambiar color de fondo de la barra de navegación
       ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(20.0),
+      body: SingleChildScrollView(
+        child: Container(
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [
+                Color.fromARGB(255, 0, 0, 0),
+                Color.fromARGB(255, 0, 0, 0),
+                Color.fromARGB(255, 0, 0, 0),
+              ],
+            ),
+          ),
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              TextField(
-                controller: emailController,
-                decoration: InputDecoration(labelText: 'Correo Electrónico'),
+              SizedBox(
+                height: 10,
               ),
-              SizedBox(height: 20),
-              TextField(
-                controller: passwordController,
-                decoration: InputDecoration(labelText: 'Contraseña'),
-                obscureText: true,
-              ),
-              SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () {
-                  final email = emailController.text;
-                  final password = passwordController.text;
-                  Navigator.pop(context, {'email': email, 'password': password});
-                },
-                child: Text('Registrarse'),
+              Image.asset('assets/logo.png'),
+              Container(
+                height: 500,
+                width: 325,
+                decoration: BoxDecoration(
+                  color: Color.fromARGB(255, 55, 64, 70),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                      height: 30,
+                    ),
+                    Text(
+                      'Registro',
+                      style: TextStyle(
+                        fontSize: 35,
+                        fontWeight: FontWeight.bold,
+                        color: Color.fromARGB(255, 0, 136, 102),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Container(
+                      width: 250,
+                      child: TextField(
+                        controller: emailController,
+                        decoration: InputDecoration(
+                          labelText: 'Correo Electrónico',
+                          labelStyle: TextStyle(color: Colors.white),
+                          suffixIcon: Icon(
+                            FontAwesomeIcons.envelope,
+                            size: 17,
+                            color: Colors.white,
+                          ),
+                          hintStyle: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    Container(
+                      width: 250,
+                      child: TextField(
+                        controller: passwordController,
+                        decoration: InputDecoration(
+                          labelText: 'Contraseña',
+                          labelStyle: TextStyle(color: Colors.white),
+                          suffixIcon: Icon(
+                            FontAwesomeIcons.eyeSlash,
+                            size: 17,
+                            color: Colors.white,
+                          ),
+                          hintStyle: TextStyle(color: Colors.white),
+                        ),
+                        obscureText: true,
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    ElevatedButton(
+                      onPressed: () {
+                        final email = emailController.text;
+                        final password = passwordController.text;
+                        Navigator.pop(context, {'email': email, 'password': password});
+                      },
+                      child: Text(
+                        'Registrarse',
+                        style: TextStyle(fontSize: 18.0),
+                      ),
+                      style: ElevatedButton.styleFrom(
+                        
+                        // Cambiar color de fondo del botón
+                        padding: EdgeInsets.symmetric(horizontal: 50, vertical: 15),
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ],
           ),
         ),
-     ),
-);
-}
+      ),
+    );
+  }
 }
